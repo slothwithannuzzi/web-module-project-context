9df290419e49e44147cb48cbb5021d54b107177d
slothwithannuzzi <78941408+slothwithannuzzi@users.noreply.github.com>
2021-06-24 10:06:16 -0400

    succesfully converted application to context api

--- src/App.js
+++ src/App.js
@@ -7,26 +7,35 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+//contexts
+import { ProductContext } from './contexts/ProductContexts';
+import { CartContext } from './contexts/CartContext'
+
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
 		// add the given item to the cart
+		setCart([...cart, item])
 	};
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+			<ProductContext.Provider value={{ products, addItem }}>
+				<CartContext.Provider value = {{cart}}>
+					<Navigation cart={cart} />
+					{/* Routes */}
+					<Route exact path="/">
+						<Products/>
+					</Route>
 
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
-
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
+					<Route path="/cart">
+						<ShoppingCart cart={cart} />
+					</Route>
+				</CartContext.Provider>
+			</ProductContext.Provider>
 		</div>
 	);
 }
